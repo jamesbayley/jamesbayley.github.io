244d288024de63e9d3dea461afa6e23016cbc480
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2022-10-04 04:13:30 +0000

    Footer icon colors change with theme.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello, jamesbayley.dev');
+// Since there are two copies of each icon in the footer 
+// so that the correct icon can be rendered after the color
+// theme is determined, sometimes the page can load before 
+// the icons have been correctly positioned, resulting in 
+// a flash on the page. To mitigate against this, the footer
+// bar is hidden from view until the DOM is fully parsed.
+window.addEventListener('load', () => {
+    document.querySelector('footer')!.style.visibility = 'visible';
+})
