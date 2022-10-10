29e5814479e941d70953efbf118de3e4eace6b99
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2022-10-10 15:01:05 +0100

    Added JS to update the viewport size if the inner window height changes to try and correct the iOS viewport behaviour as the toolbar opens/closes.

--- static/main.js
+++ static/main.js
@@ -7,3 +7,11 @@
 window.addEventListener('load', () => {
     document.querySelector('footer').style.visibility = 'visible';
 })
+
+// iOS Safari's toolbar affects the viewport dimensions for the 
+// screen. 100vh is initially set assuming that the toolbar is 
+// hidden. However, if the toolbar is displayed then the main
+// content of the window is smaller than expected. This negatively
+// impacts the layout and visibility of elements on screen. 
+// Accordingly, vh must be recalculated when the viewport changes.
+window.onresize = () => document.body.height = window.innerHeight;
