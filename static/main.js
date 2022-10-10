ac62cbbaaf8e7b73da71ddbc0448ab33b052a1b9
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2022-10-10 15:18:35 +0100

    Attempting pure CSS approach to the iOS viewport issue using a webkit specific CSS property.

--- static/main.js
+++ static/main.js
@@ -8,17 +8,17 @@ window.addEventListener('load', () => {
     document.querySelector('footer').style.visibility = 'visible';
 })
 
-// iOS Safari's toolbar affects the viewport dimensions for the 
-// screen. 100vh is initially set assuming that the toolbar is 
+// iOS Safari's toolbar affects the viewport dimensions for the
+// screen. 100vh is initially set assuming that the toolbar is
 // hidden. However, if the toolbar is displayed then the main
 // content of the window is smaller than expected. This negatively
-// impacts the layout and visibility of elements on screen. 
+// impacts the layout and visibility of elements on screen.
 // Accordingly, vh must be recalculated everytime vh changes.
-const updateViewportHeight = () => document.body.height = window.innerHeight;
-window.addEventListener('resize', () => updateViewportHeight);
+// const updateViewportHeight = () => document.body.height = window.innerHeight;
+// window.addEventListener('resize', () => updateViewportHeight);
 
-// The resize event must be called immediately on page load, so that 
+// The resize event must be called immediately on page load, so that
 // the initial dimensions of the iOS toolbar can be accounted for.
 // Subsequent re-sizings are automatically detected by the 'onresize'
 // event listener.
-window.addEventListener('load', updateViewportHeight);
+// window.addEventListener('load', updateViewportHeight);
