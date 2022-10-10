2ba6cb19fea6f570a8535c6bbcadd1dde6d33922
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2022-10-10 15:35:00 +0100

    Updated JavaScript to update height of main element on resize and page load.

--- static/main.js
+++ static/main.js
@@ -1,3 +1,7 @@
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const main = document.querySelector('main');
+
 // Since there are two copies of each icon in the footer 
 // so that the correct icon can be rendered after the color
 // theme is determined, sometimes the page can load before 
@@ -14,11 +18,16 @@ window.addEventListener('load', () => {
 // content of the window is smaller than expected. This negatively
 // impacts the layout and visibility of elements on screen.
 // Accordingly, vh must be recalculated everytime vh changes.
-// const updateViewportHeight = () => document.body.height = window.innerHeight;
-// window.addEventListener('resize', () => updateViewportHeight);
-
+//
 // The resize event must be called immediately on page load, so that
 // the initial dimensions of the iOS toolbar can be accounted for.
 // Subsequent re-sizings are automatically detected by the 'onresize'
 // event listener.
-// window.addEventListener('load', updateViewportHeight);
+function updateMainHeight() {
+    const headerHeight = header.style.height;
+    const footerHeight = footer.style.height;
+    main.style.height = window.innerHeight - headerHeight - footerHeight;
+}
+
+window.addEventListener('resize', updateMainHeight);
+window.addEventListener('load', updateMainHeight);
